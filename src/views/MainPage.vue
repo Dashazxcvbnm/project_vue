1e37516304ceee061f0c1f0cf35e23850bcee8a5
Dasha <dashalis1402@gmail.com>
2023-01-26 19:16:07 +0300

    Сазданна

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>Главная страница {{ userName }} </h1>
+        <h1>Доброго времени суток! Нажми на кнопочку и {{ userName }} расскажет о себе</h1>
+        <!-- Вариант перехода на станицу для кнопки -->
+        <button @click="$router.push('/about')">Я {{ userName }}, тут обо мне</button>
+
+        <!-- Вариант ссылки из документации через копонент router-link -->
+        <!-- <router-link to="/about">Я {{ userName }}, тут обо мне</router-link> -->
+
     </div>
 </template>
 
